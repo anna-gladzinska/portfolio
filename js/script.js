c3d69a739794e7afd59eb002af35b0cfdbe47ee9
Anna Gladzińska <agladzinska@gmail.com>
2020-07-13 17:53:35 +0200

    Add bg transition to nav

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ toggle.addEventListener('click', function () {
 menu.addEventListener('click', function () {
   menu.classList.remove('dropdown-show');
 })
+
+const nav = document.querySelector('nav');
+console.log(nav.scrollTop);
+
+document.addEventListener("scroll", function () {
+  // console.log(window.scrollY);
+  if (window.scrollY > 0) {
+    nav.classList.add('nav-bg');
+  } else {
+    nav.classList.remove('nav-bg');
+  }
+});
