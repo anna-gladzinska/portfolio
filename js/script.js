ce8a7a88486d6d97faca218e3a27178b90ee4f95
Anna Gladzińska <agladzinska@gmail.com>
2020-07-14 19:11:17 +0200

    Minor nav change

--- js/script.js
+++ js/script.js
@@ -10,14 +10,17 @@ menu.addEventListener('click', function () {
 
 const nav = document.querySelector('nav');
 const ul = document.querySelector('.nav-ul');
+const navigation = document.querySelector('.navigation div');
 
 document.addEventListener("scroll", function () {
   if (window.scrollY > 0) {
     nav.classList.add('nav-bg');
     ul.classList.add('nav-ul-show');
+    navigation.classList.add('title-left');
   } else {
     nav.classList.remove('nav-bg');
     ul.classList.remove('nav-ul-show');
+    navigation.classList.remove('title-left');
   }
 });
 
